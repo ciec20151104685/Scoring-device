9e7f3c825db965da088936a8525803b6f48c7302
ciec20151104685 <s20151104685@admindeiMac-38.local>
2017-05-09 20:53:00 +0800

    新版本
    修改了字体大小，添加了弹出对话框，能判断输赢  未加入详细规则

--- Scoring device/ViewController.swift
+++ Scoring device/ViewController.swift
@@ -24,35 +24,70 @@ class ViewController: UIViewController {
     @IBOutlet weak var displayB: UITextField!
     var a = 0
     var b = 0
- if a<0
-    {
-    
-    }
-    func showAlert(){
-        let alert = UIAlertView(title: "提示", message: "小心身后", delegate: self, cancelButtonTitle: "确定")
+    func showAlertWorry(){
+        let alert = UIAlertView(title: "错误", message: "分数不可低于零", delegate: self, cancelButtonTitle: "Fuck")
         alert.alertViewStyle = UIAlertViewStyle.default
         
         alert.show()
     }
+    func showAlertAWin(){
+        let alert = UIAlertView(title: "恭喜", message: "a赢了", delegate: self, cancelButtonTitle: "重新开始")
+        alert.alertViewStyle = UIAlertViewStyle.default
+        
+        alert.show()
+        a=0
+        displayA.text =  "\(a)"
+    }
+    func showAlertBWin(){
+        let alert = UIAlertView(title: "恭喜", message: "b赢了", delegate: self, cancelButtonTitle: "重新开始")
+        alert.alertViewStyle = UIAlertViewStyle.default
+        
+        alert.show()
+        b=0
+        displayB.text =  "\(b)"
+    }
+    
     @IBAction func addA(_ sender: UIButton) {
         a=a+1
         displayA.text = "\(a)"
+        if a==8{
+            showAlertAWin()
+        }
      
     }
     @IBAction func reduceA(_ sender: UIButton) {
         a=a-1
         displayA.text = "\(a)"
+        if a < 0 {
+            
+            showAlertWorry()
+            a=0
+            displayA.text =  "\(a)"
+        }
         
     }
     @IBAction func addB(_ sender: Any) {
         b=b+1
-        displayB.text = "\(b)"
+        displayB.text =  "\(b)"
+        if b == 8{
+        showAlertBWin()
+        }
     }
     
     @IBAction func reduceB(_ sender: UIButton) {
         b=b-1
         displayB.text = "\(b)"
+        if b < 0 {
+            showAlertWorry()
+            b=0
+            displayA.text =  "\(b)"
+        }
 
+    }
+    
+    @IBAction func begangame(_ sender: UIButton) {
+        displayB.text="\(b)"
+        displayA.text="\(a)"
     }
   
     
